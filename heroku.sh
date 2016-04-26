efbdb46030f681f9c436dad182654fa2771474e3
Nathan Totten <ntotten@auth0.com>
2016-04-26 11:31:25 -0700

    adsfads

--- heroku.sh
+++ heroku.sh
@@ -6,5 +6,6 @@ shopt -s extglob dotglob
 mv !(docs) docs
 shopt -u dotglob
 git clone https://$GITHUB_USER_NAME:$GITHUB_ACCESS_TOKEN@github.com/auth0/auth0-docs.git auth0-docs-repo
-#shopt -s dotglob nullglob; mv auth0-docs-repo/* .
+shopt -s dotglob nullglob;
+mv auth0-docs-repo/* .
 #rmdir auth0-docs-repo
