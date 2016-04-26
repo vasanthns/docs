e1edda04751bf8451a08da5fc950a5b18523b9f3
Nathan Totten <ntotten@auth0.com>
2016-04-26 11:25:57 -0700

    adsfads

--- heroku.sh
+++ heroku.sh
@@ -2,7 +2,7 @@
 
 
 mkdir -p "./docs/"
-find ./ -name docs -prune -print0 | xargs -0 mv --target-directory=/docs
+find ./ -name docs -prune -print0 | xargs -0 mv --target-directory=./docs
 git clone https://$GITHUB_USER_NAME:$GITHUB_ACCESS_TOKEN@github.com/auth0/auth0-docs.git auth0-docs-repo
 shopt -s dotglob nullglob; mv auth0-docs-repo/* .
 #rmdir auth0-docs-repo
