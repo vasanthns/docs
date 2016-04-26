1dd5752c02430739cbb3aa441c5dfe7df28e5407
Nathan Totten <ntotten@auth0.com>
2016-04-26 11:15:07 -0700

    Fafdsafds

--- heroku.sh
+++ heroku.sh
@@ -3,3 +3,6 @@
 
 mkdir -p "./docs/"
 mv ./*.* ./docs/
+git clone https://$GITHUB_USER_NAME:$GITHUB_ACCESS_TOKEN@github.com/auth0/auth0-docs.git auth0-docs-repo
+shopt -s dotglob nullglob; mv auth0-docs-repo/* .
+rmdir auth0-docs-repo
