00ee97800ba39cdee4ae1dc912f02bcbd6de5559
Nathan Totten <ntotten@auth0.com>
2016-04-26 11:55:30 -0700

    Foo

--- heroku.sh
+++ heroku.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 
+echo "Moving content into docs folder"
 mkdir -p "./docs/"
 shopt -s extglob dotglob
 mv !(docs) docs
-shopt -u dotglob
 git clone https://$GITHUB_USER_NAME:$GITHUB_ACCESS_TOKEN@github.com/auth0/auth0-docs.git auth0-docs-repo
-shopt -s dotglob nullglob;
+
+echo "Moving docs site into root folder"
 mv auth0-docs-repo/* .
 #rmdir auth0-docs-repo
+shopt -u dotglob
+
+echo "Docs site successfully setup."
