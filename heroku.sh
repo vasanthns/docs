b3658612ce8342aadf74b5b13871327cf896692a
Nathan Totten <ntotten@auth0.com>
2016-04-26 12:37:42 -0700

    fdsf

--- heroku.sh
+++ heroku.sh
@@ -4,6 +4,7 @@
 echo "Moving content into docs folder"
 mkdir -p "./docs/"
 mv !(docs) docs
+
 git clone https://$GITHUB_USER_NAME:$GITHUB_ACCESS_TOKEN@github.com/auth0/auth0-docs.git auth0-docs-repo
 
 echo "Moving docs site into root folder"
